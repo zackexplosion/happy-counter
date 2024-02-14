8fc0c785a5ea59eaf8044634f43de8631cbeaa0f
Zack <zack@zackexplosion.fun>
2024-02-15 02:45:59 +0800

    Add github corner link and some changes

--- src/App.js
+++ src/App.js
@@ -2,51 +2,55 @@ import React from 'react'
 import './App.css';
 const TIME_OF_HAPPY = '2024/03/01';
 
-const CounterView = props =>{
-  const { days, hours, minutes, seconds } = props
+const CounterView = props => {
+  const { days, hours, minutes, seconds, is_happy } = props
+
+  if (is_happy) {
+    return false
+  }
   return (
     <div>
       <div className="csvg-digit"
-          data-tad-bind="days">
+        data-tad-bind="days">
         <div className="csvg-digit-number"
-            id="el_dw1">
+          id="el_dw1">
           {days}
         </div>
         <div className="csvg-digit-label"
-            id="el_dw1t">
+          id="el_dw1t">
           days
         </div>
       </div>
       <div className="csvg-digit"
-          data-tad-bind="hours">
+        data-tad-bind="hours">
         <div className="csvg-digit-number"
-            id="el_h1">
+          id="el_h1">
           {hours}
         </div>
         <div className="csvg-digit-label"
-            id="el_h1t">
+          id="el_h1t">
           hours
         </div>
       </div>
       <div className="csvg-digit"
-          data-tad-bind="minutes">
+        data-tad-bind="minutes">
         <div className="csvg-digit-number"
-            id="el_m1">
+          id="el_m1">
           {minutes}
         </div>
         <div className="csvg-digit-label"
-            id="el_m1t">
+          id="el_m1t">
           minutes
         </div>
       </div>
       <div className="csvg-digit"
-          data-tad-bind="seconds">
+        data-tad-bind="seconds">
         <div className="csvg-digit-number"
-            id="el_s1">
+          id="el_s1">
           {seconds}
         </div>
         <div className="csvg-digit-label"
-            id="el_s1t">
+          id="el_s1t">
           seconds
         </div>
       </div>
@@ -64,6 +68,10 @@ const getDistance = () => {
   // Find the distance between now and the date from
   const distance = countFromDate - now
 
+  if (distance <= 0) {
+    return false
+  }
+
   // Time calculations for days, hours, minutes and seconds
   const days = Math.floor(distance / (1000 * 60 * 60 * 24))
   const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
@@ -74,7 +82,7 @@ const getDistance = () => {
 }
 
 class Counter extends React.Component {
-// class Counter {
+  // class Counter {
   constructor(props) {
     super(props)
     this.state = {
@@ -82,18 +90,23 @@ class Counter extends React.Component {
       hours: 0,
       minutes: 0,
       seconds: 0,
+      is_happy: false,
     }
   }
 
   updateCounter() {
     const t = getDistance()
 
-    this.setState({
-      days: t[0],
-      hours: t[1],
-      minutes: t[2],
-      seconds: t[3],
-    })
+    if (t === false) {
+      this.setState({ is_happy: true })
+    } else {
+      this.setState({
+        days: t[0],
+        hours: t[1],
+        minutes: t[2],
+        seconds: t[3],
+      })
+    }
   }
 
   componentDidMount() {
@@ -119,6 +132,7 @@ class Counter extends React.Component {
         // width: '640',
         videoId: '9jK-NcRmVcw',
         playerVars: {
+          'autoplay': true,
           'playsinline': 1,
           'start': 117,
         },
@@ -147,9 +161,9 @@ function App() {
         {/* <AudioPlayer src={BGM} ready={ready}/> */}
         {/* <img className="screenshot" src={screenshot} alt="black and white screenshot"/> */}
         <h1 className="csvg-title font-cursive title">
-        Happy working in Academia Sinica!
+          Happy working in Academia Sinica!
         </h1>
-        <Counter/>
+        <Counter />
         <div id="player"></div>
       </div>
       {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/9jK-NcRmVcw?si=IrvbggH-FUoYbr3d&amp;controls=0"></iframe> */}
